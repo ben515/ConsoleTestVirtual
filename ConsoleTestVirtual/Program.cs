f51bf059d1e99e8953626380d35e81c4faec8575
名賢 邱 <love954009@gmail.com>
2018-05-04 14:00:17 +0800

    V1沒有Virtual 練習有Virtual與沒有的差別

---
+++ ConsoleTestVirtual/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTestVirtual
+{
+    class A
+    {
+        public void Print()
+        {
+            Console.WriteLine("This is A.print");
+        }
+    }
+    class B :A
+    {
+        public new void Print()
+        {
+            Console.WriteLine("This is B.Print");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A obj = new B();
+            obj.Print();
+            Console.ReadKey();
+
+        }
+    }
+}
